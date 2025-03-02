cfee3105dbcd68a489931e77840e125441e66434
jypang0 <162472682+jypang0@users.noreply.github.com>
2025-03-03 08:38:00 +0900

    Time: 7 ms (96.67%), Space: 58.9 MB (22.96%) - LeetHub

--- 1896-maximum-score-from-performing-multiplication-operations/1896-maximum-score-from-performing-multiplication-operations.java
+++ 1896-maximum-score-from-performing-multiplication-operations/1896-maximum-score-from-performing-multiplication-operations.java
@@ -1,33 +1,53 @@
 class Solution {
 
-    private int[][] memo;
-    private int[] nums, multipliers;
-    private int n, m;
-
-    private int dp(int i, int left) {
-        // base - completed all operations + no more to choose from multipliers
-        if (i == m) {
-            return 0;
-        }
-
-        int right = n - 1 - (i - left);
-        int mult = multipliers[i];
-
-        // relation
-        if (memo[i][left] == 0) {
-            memo[i][left] = Math.max(mult * nums[left] + dp(i + 1, left + 1), mult * nums[right] + dp(i + 1, left));
-        }
-
-        return memo[i][left];
-    }
-
+    // bottom up
     public int maximumScore(int[] nums, int[] multipliers) {
-        this.n = nums.length;
-        this.m = multipliers.length;
-        this.nums = nums;
-        this.multipliers = multipliers;
-        this.memo = new int[m][m];
+        int m = multipliers.length;
+        int n = nums.length;
 
-        return dp(0, 0);
+        int[][] dp = new int[m+1][m+1];
+
+        for (int i = m-1; i >= 0; i--) {
+            int mult = multipliers[i];
+            for (int left = i; left >= 0; left--) {
+                int right = n-1-(i-left);
+                dp[i][left] = Math.max(mult*nums[left]+dp[i+1][left+1], mult*nums[right]+dp[i+1][left]);
+            }
+        }
+
+        return dp[0][0];
     }
+
+    // // top down
+    // private int[][] memo;
+    // private int[] nums, multipliers;
+    // private int n, m;
+
+    // private int dp(int i, int left) {
+    // // base - completed all operations + no more to choose from multipliers
+    // if (i == m) {
+    // return 0;
+    // }
+
+    // int right = n - 1 - (i - left);
+    // int mult = multipliers[i];
+
+    // // relation
+    // if (memo[i][left] == 0) {
+    // memo[i][left] = Math.max(mult * nums[left] + dp(i + 1, left + 1), mult *
+    // nums[right] + dp(i + 1, left));
+    // }
+
+    // return memo[i][left];
+    // }
+
+    // public int maximumScore(int[] nums, int[] multipliers) {
+    // this.n = nums.length;
+    // this.m = multipliers.length;
+    // this.nums = nums;
+    // this.multipliers = multipliers;
+    // this.memo = new int[m][m];
+
+    // return dp(0, 0);
+    // }
 }
