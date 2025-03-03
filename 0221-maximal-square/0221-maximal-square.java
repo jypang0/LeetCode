f3af8befb59129283c2fe951023c710322bc4fa0
jypang0 <162472682+jypang0@users.noreply.github.com>
2025-03-03 13:02:42 +0900

    Time: 5 ms (99.29%), Space: 60.3 MB (57.63%) - LeetHub

---
+++ 0221-maximal-square/0221-maximal-square.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int answer = 0;
+
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i][j] = matrix[i][j] - '0';
+                if (dp[i][j] == 1) {
+                    answer = 1;
+                }
+            }
+        }
+
+        for (int i = m - 2; i >= 0; i--) {
+            for (int j = n - 2; j >= 0; j--) {
+                if (dp[i][j] == 0) {
+                    continue;
+                }
+
+                int right = dp[i][j + 1];
+                int bottom = dp[i + 1][j];
+                int diag = dp[i + 1][j + 1];
+
+                if (right == 0 || bottom == 0 || diag == 0) {
+                    continue;
+                }
+
+                dp[i][j] = Math.min(right, Math.min(bottom, diag)) + 1;
+
+                answer = Math.max(dp[i][j] * dp[i][j], answer);
+            }
+        }
+
+        return answer;
+    }
+}
