a285a182eff39d5b34ef26b2fec9b1ac07b7dd0a
jypang0 <162472682+jypang0@users.noreply.github.com>
2025-03-12 16:56:33 +0900

    Time: 2 ms (65.12%), Space: 41.4 MB (72.61%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,26 +1,37 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
+        int sP = 0;
+        int tP = 0;
 
-        int sL = s.length();
-        int tL = t.length();
-
-        if(sL==0) return true;
-        if(sL>tL) return false;
-
-        // s's index
-        int sI = 0;
-        char cur = s.charAt(sI);
-
-        for(int tI=0;tI<tL;tI++) {
-            if(cur==t.charAt(tI)) {
-                sI++;
-                if(sI==sL) {
-                    return true;
-                }
-                cur = s.charAt(sI);
-            }
+        while(sP<s.length() && tP<t.length()) {
+            if(s.charAt(sP)==t.charAt(tP)) sP++;
+            tP++;
         }
 
-        return false;
+        return sP==s.length();
     }
+    // public boolean isSubsequence(String s, String t) {
+
+    //     int sL = s.length();
+    //     int tL = t.length();
+
+    //     if(sL==0) return true;
+    //     if(sL>tL) return false;
+
+    //     // s's index
+    //     int sI = 0;
+    //     char cur = s.charAt(sI);
+
+    //     for(int tI=0;tI<tL;tI++) {
+    //         if(cur==t.charAt(tI)) {
+    //             sI++;
+    //             if(sI==sL) {
+    //                 return true;
+    //             }
+    //             cur = s.charAt(sI);
+    //         }
+    //     }
+
+    //     return false;
+    // }
 }
